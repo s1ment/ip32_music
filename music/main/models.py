75075fda566d2a42565190f0c498c08227d1a21a
KulKuksh <veysman7k@mail.ru>
2026-03-26 13:05:00 +0700

    добавлена модель исполнителя

--- music/main/models.py
+++ music/main/models.py
@@ -4,13 +4,23 @@ class Genres(models.Model):
     name_ru = models.CharField(max_length = 200, unique = True)
     name_en = models.CharField(max_length = 200)
     description = models.CharField(max_length = 400)
+    
     def __str__(self):
         return self.name_en
+
+class Artist(models.Model):
+    name = models.CharField(max_length=500, unique=True)
+    image = models.ImageField(upload_to='artists/', null=True)
+
+    def __str__(self):
+        return self.name
     
 class Track(models.Model):
     title = models.CharField(max_length = 200, unique = True)
     duration = models.CharField(max_length = 200)
     genre = models.ManyToManyField(Genres)
+    artist = models.ForeignKey(Artist, on_delete=models.RESTRICT, null=True)
+
     def __str__(self):
         return self.title
     
