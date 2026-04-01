2eecf9575d9364fc0f99f65b83307fcca3f23bef
Maxim <maxim@gmail.com>
2026-04-01 13:26:21 +0700

    Сделал исполнителей

--- music/main/views.py
+++ music/main/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Genres
 from .models import Track, Artist
 from .forms import GenreForm, ArtistForm
+
 def index (request):
     genres1 = Genres.objects.all
     return render (request, 'index.html', {'genres1': genres1})
@@ -32,7 +33,6 @@ def add_genre(request):
 def delete_genre(request, id_genre):
     genre = Genres.objects.get(id=id_genre)
     genre.delete()
-   
 
 def edit_genre(request, id_genre):
     g = Genres.objects.get(id=id_genre)
@@ -44,3 +44,50 @@ def edit_genre(request, id_genre):
     else:
         form = GenreForm(instance=g)
     return render(request, "edit_genre.html", {"form": form})
+
+def track(request):
+    artists = Artist.objects.all()
+    tracks = Track.objects.all()
+    selected_artist = None
+    if request.method == "POST":
+        artist_id = request.POST.get('artist')
+        if artist_id:
+            try:
+                selected_artist = Artist.objects.get(id=artist_id)
+                tracks = Track.objects.filter(artist=selected_artist)
+            except Artist.DoesNotExist:
+                tracks = Track.objects.none()
+    context = {
+        'track1': tracks,
+        'artists': artists,
+        'selected_artist': selected_artist,
+    }
+    return render(request, 'track.html', context)
+
+def add_artist(request):
+    if request.method == "POST":
+        form = ArtistForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('artists')
+    else:
+        form = ArtistForm()
+    return render(request, 'add_artist.html', {'form': form})
+
+def edit_artist(request, artist_id):
+    artist = get_object_or_404(Artist, id=artist_id)
+    if request.method == "POST":
+        form = ArtistForm(request.POST, request.FILES, instance=artist)
+        if form.is_valid():
+            form.save()
+            return redirect('artists')
+    else:
+        form = ArtistForm(instance=artist)
+    return render(request, 'edit_artist.html', {'form': form, 'artist': artist})
+
+def delete_artist(request, artist_id):
+    artist = get_object_or_404(Artist, id=artist_id)
+    if request.method == "POST":
+        artist.delete()
+        return redirect('artists')
+    return render(request, 'delete_artist.html', {'artist': artist})
--- music/music/urls.py
+++ music/music/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from main import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,7 +10,12 @@ urlpatterns = [
     path('index', views.index),
     path('genres/', views.genres),
     path('track/', views.track),
-    path('artists/', views.artists),
     path('add_genre/', views.add_genre),
     path('editgenre/<int:id_genre>', views.edit_genre),
+    path('artists/', views.artists, name='artists'),
+    path('add_artist/', views.add_artist, name='add_artist'),
+    path('edit_artist/<int:artist_id>/', views.edit_artist, name='edit_artist'),
+    path('delete_artist/<int:artist_id>/', views.delete_artist, name='delete_artist'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
